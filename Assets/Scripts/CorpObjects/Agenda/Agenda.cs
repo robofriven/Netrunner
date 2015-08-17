5c8b29a3c5bab109721e687a9e7f0c0eea96a1da
robofriven <robofriven@yahoo.com>
2015-08-16 21:21:36 -0700

    Set up Agenda System

---
+++ Assets/Scripts/CorpObjects/Agenda/Agenda.cs
@@ -0,0 +1,44 @@
+﻿// When building this class
+
+using UnityEngine;
+using System.Collections;
+
+public class Agenda : CorpObject {
+
+    new public string name;
+    public string description;
+    public int points;
+    public int advance;
+    public int completionCost;
+    public int completionPoints;
+    public bool complete;
+    public Corp corp;
+
+    void Start()
+    {
+        advance = 0;
+        complete = false;
+
+        corp = GameObject.FindObjectOfType(typeof(Corp)) as Corp;
+    }
+
+    public void advanceAgenda()
+    {
+        if (corp.credits >= 1)
+        {
+            corp.credits --;
+            advance ++;
+        }
+
+
+        if (advance == completionCost)
+        {
+            // Do whatever the agenda does on completion
+        }
+    }
+
+    public virtual void activate()
+    {
+        Debug.LogWarning("Agenda's activate() method not implemented.");
+    }
+}
---
+++ Assets/Scripts/CorpObjects/Agenda/PayAgenda.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PayAgenda : Agenda {
+
+    public int pay;
+
+	void Start () 
+    {
+        name = "Agenda Payment";
+        description = "This will pay 'pay' credits upon completion";
+	}
+
+    public void build(int pay, int cost, int points)
+    {
+        this.pay = pay;
+        this.completionCost = cost;
+    }
+
+    public void activate()
+    {
+        corp.agendaPts += points;
+        corp.credits += pay;
+    }
+	
+}
